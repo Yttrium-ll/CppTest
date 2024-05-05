d90266b8f3fc958bd5ce389d725da722bb2fb3e4
kuriache <kuser2021@mail.ru>
2024-05-05 19:07:13 +0300

    Добавление простых программ

---
+++ CppTest1/CppTest1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+#include <locale>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	/*int number_1;
+	int number_2;
+	cout << "Введите число ";
+	cin >> number_1;
+	cout << "Введите число ";
+	cin >> number_2;
+
+	if (number_1 % number_2 == 0)
+	{
+		cout << number_1 << " делится на "<< number_2 << endl;
+	}
+	else
+	{
+		cout << number_1 << " не делится на  " << number_2 << endl;
+	}*/
+	float a;
+	float c = 0;
+	for (float b = 0; b < 10; b++)
+	{
+		cout << "Введите число ";
+		cin >> a;
+		c = c + a;
+
+	}
+	cout << c << endl;
+
+
+}
+
